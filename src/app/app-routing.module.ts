7c7b21ae39ba849cd5c56b55527e78301e06d5f3
Franco <francosanchezkpo@gmail.com>
2020-03-25 14:01:41 -0300

    trate de hacer una branch y casi me da un infarto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './components/posts/post/post.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {
+    path:'home',
+    loadChildren:()=>
+      import('./components/pages/home/home.module').then(m => m.HomeModule)
+  },
+  { path: 'post/:id', component: PostComponent},
+  { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) }
+];
 
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
